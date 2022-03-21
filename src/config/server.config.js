717e2de84c50c13025d690de27b9f4c1cc8e24b9
sharifrafid <sharifrafidurrahman@gmail.com>
2022-03-21 07:58:38 +0600

    Changed Folder Structure

--- src/config/server.config.js
+++
@@ -1,7 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-const APP_CONFIG = {
-    ENV: !process.env.ENV ? 'development' : process.env.ENV,
-    PORT: !process.env.PORT ? '3000' : process.env.PORT
-}
-export default APP_CONFIG
